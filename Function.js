0dd958be3b793b82853b84d28e06d7ef61bea53a
Krish Ghori <krishghori90@gmail.com>
2026-04-01 18:04:21 +0530

    add arrow function getmyname

---
+++ Function.js
@@ -0,0 +1,9 @@
+function printnumbers(num){
+    console.log(num);
+}
+printnumbers(5);
+
+let getmyname = (fristname,lastname) =>{
+    let fullname = fristname+ " " +lastname;
+    return fullname;
+}  
