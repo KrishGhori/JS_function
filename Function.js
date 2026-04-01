33b90b6141ec4c678756d07baf4b7a187310533c
Krish Ghori <krishghori90@gmail.com>
2026-04-01 18:07:25 +0530

    add function call, log full name, and create object with properties and method

--- Function.js
+++ Function.js
@@ -7,3 +7,16 @@ let getmyname = (fristname,lastname) =>{
     let fullname = fristname+ " " +lastname;
     return fullname;
 }  
+
+let myname=getmyname("krish","ghori");
+console.log(myname);
+ 
+//object
+let obj = {
+    name:"krish",
+    "full name":"hii hello", //if you are give to space on a object so that's write in a (" ")
+    age:19,
+    call : function(){
+        console.log("hello, hanji numste!");
+    }
+};
